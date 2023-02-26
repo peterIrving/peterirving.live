24bf0e2bbd1b2e3def7c98a5bcf7a7b1aadacd0c
Peter Irving <peter@northernground.com>
2023-02-25 18:23:53 -0600

    added spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const pageStyles = {
   backgroundImage: `url(${require("../images/web_background_white.png").default})`,
   // backgroundSize: "cover",
   backgroundSize: "100% auto",
-  backgroundRepeat:"repeat-y",
+  backgroundRepeat: "repeat-y",
 }
 
 const centered = {
@@ -17,8 +17,8 @@ const centered = {
 } as const;
 
 const headingStyles = {
-  // marginTop: 0,
-  // marginBottom: 64,
+  marginTop: 64,
+  marginBottom: 64,
   // maxWidth: 320,
   // fontFamily: ["Sancreek", 'cursive',],
   // fontFamily: "Sancreek",
@@ -67,6 +67,7 @@ const docLinkStyle = {
   display: `inline-block`,
   marginBottom: 24,
   marginRight: 12,
+
 }
 
 const descriptionStyle = {
@@ -114,6 +115,7 @@ const IndexPage: React.FC<PageProps> = () => {
         Peter Irving
       </h1>
       <h3 style={headingAccentStyles}>Generally Steppy Folk Music</h3>
+
       <h1 style={headingStyles}>
         Peter Irving
       </h1>
@@ -162,7 +164,7 @@ const IndexPage: React.FC<PageProps> = () => {
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       /> */}
-    </main>
+    </main >
   )
 }
 
